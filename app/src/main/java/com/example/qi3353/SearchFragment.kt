cdbf3e141a7d5370b1c31c83f5ebe33851098cc3
Feliica <seofelicia@vt.edu>
2023-03-02 17:00:49 -0500

    up to the fyp frag is viewable on run

--- app/src/main/java/com/example/qi3353/SearchFragment.kt
+++ app/src/main/java/com/example/qi3353/SearchFragment.kt
@@ -10,7 +10,7 @@ import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.qi3353.databinding.FragmentSearchBinding
-import com.example.qi3353.databinding.TagSearchItemsBinding
+import com.example.qi3353.databinding.TagSearchItemBinding
 
 class SearchFragment : Fragment() {
     private lateinit var binding: FragmentSearchBinding
@@ -42,10 +42,10 @@ class SearchFragment : Fragment() {
 
 class SearchRVA(private val tags: List<String>, private val activity: MainActivity) :
     RecyclerView.Adapter<SearchRVA.ViewHolder>() {
-    private lateinit var binding: TagSearchItemsBinding
+    private lateinit var binding: TagSearchItemBinding
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchRVA.ViewHolder {
-        binding = TagSearchItemsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        binding = TagSearchItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         val view = binding.root
         return ViewHolder(view, binding, activity)
     }
@@ -56,7 +56,7 @@ class SearchRVA(private val tags: List<String>, private val activity: MainActivi
 
     override fun getItemCount() = tags.size
 
-    class ViewHolder(private val view: View, private val binding: TagSearchItemsBinding, private val activity: MainActivity) :
+    class ViewHolder(private val view: View, private val binding: TagSearchItemBinding, private val activity: MainActivity) :
         RecyclerView.ViewHolder(view) {
         fun bindItems(tag: String) {
             binding.tag.text = tag
