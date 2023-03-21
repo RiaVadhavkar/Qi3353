111d007bc16a33544c1cea6b2761a5539f36b822
Feliica <seofelicia@vt.edu>
2023-03-21 16:59:56 -0400

    spinner for reminder

--- app/src/main/java/com/example/qi3353/ProfileFragment.kt
+++ app/src/main/java/com/example/qi3353/ProfileFragment.kt
@@ -1,10 +1,13 @@
 package com.example.qi3353
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.navigation.findNavController
 import com.example.qi3353.databinding.FragmentProfileBinding
 
@@ -31,6 +34,24 @@ class ProfileFragment : Fragment() {
         }
         binding.preferencesButton.setOnClickListener {
             view.findNavController().navigate(R.id.action_profileFragment_to_preferencesFragment)
+        }
+
+        val spinner = binding.reminderSpinner
+        ArrayAdapter.createFromResource(
+            activity as Context,
+            R.array.reminderTimes,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
+        spinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+            }
+            override fun onNothingSelected(adapterView: AdapterView<*>?) {
+            }
         }
 
         return view
