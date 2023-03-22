7714637d50f35f5c81f09bc21af4149b11785ffc
Feliica <seofelicia@vt.edu>
2023-03-22 15:30:59 -0400

    continue w google

--- app/src/main/java/com/example/qi3353/SignInFragment.kt
+++ app/src/main/java/com/example/qi3353/SignInFragment.kt
@@ -1,6 +1,5 @@
 package com.example.qi3353
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -8,111 +7,94 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.findNavController
 import com.example.qi3353.databinding.FragmentSignInBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+
+const val REQUEST_CODE = 0
 
 class SignInFragment : Fragment() {
-    private lateinit var auth: FirebaseAuth
-    private lateinit var view: View
-    private lateinit var googleSignInClient: GoogleSignInClient
-
     private lateinit var binding: FragmentSignInBinding
+    private lateinit var view: View
+    private lateinit var auth: FirebaseAuth
+    private lateinit var client: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSignInBinding.inflate(inflater, container, false)
-        val view = binding.root
+        view = binding.root
 
-        var signInButton = binding.signInWithGoogle
+        auth = FirebaseAuth.getInstance()
 
-        auth = Firebase.auth
+        val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
 
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+        client = GoogleSignIn.getClient(view.context, options)
 
-        googleSignInClient = GoogleSignIn.getClient(view.context, gso)
-
-        signInButton.setOnClickListener{
-            signInGoogle()
+        binding.signInWithGoogle.setOnClickListener {
+            signInWithGoogle()
         }
 
-        // TEMPORARY!
-        binding.signInWithGoogle.setOnClickListener{
-            view.findNavController().navigate(R.id.action_signInFragment_to_preferencesFragment)
-        }
-
-        // place holder for now (need to change later on)
-        binding.continueAsGuest.setOnClickListener{
+        binding.continueAsGuest.setOnClickListener {
             view.findNavController().navigate(R.id.action_signInFragment_to_forYouFragment)
         }
 
         return view
     }
 
-    private fun signInGoogle() {
-        val signInIntent = googleSignInClient.signInIntent
-        Log.e("error", "test1")
-        launcher.launch(signInIntent)
+    private fun signInWithGoogle() {
+        val signInIntent = client.signInIntent
+        startActivityForResult(signInIntent, REQUEST_CODE)
     }
 
-
-    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            result ->
-            if (result.resultCode == Activity.RESULT_OK){
-                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-                handleResults(task)
-            }else{
-                Log.e("error", result.resultCode.toString())
-                Activity.RESULT_CANCELED
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            if (task.isSuccessful) {
+                val account = task.result
+                if (account != null) {
+                    updateUI(account)
+                }
+                Log.d("ERROR (SignInFragment.kt): ",
+                    "onActivityResult() => No Account")
             }
-
-    }
-
-    private fun handleResults(task: Task<GoogleSignInAccount>) {
-        if (task.isSuccessful){
-            Log.e("error", "test2")
-            val account : GoogleSignInAccount? = task.result
-            if (account != null){
-                updateUI(account)
+            else {
+                Log.d("ERROR (SignInFragment.kt): ",
+                    "onActivityResult() => " + task.exception.toString())
             }
         }
-        else{
-            Log.e("error", "test3")
-            Toast.makeText(activity, task.exception.toString(), Toast.LENGTH_SHORT).show()
+        else {
+            Log.d("ERROR (SignInFragment.kt): ",
+                "onActivityResult() => Result Code (" + resultCode + ")")
         }
     }
 
     private fun updateUI(account: GoogleSignInAccount) {
         val credential = GoogleAuthProvider.getCredential(account.idToken, null)
         auth.signInWithCredential(credential).addOnCompleteListener {
-            if (it.isSuccessful){
-                val intent : Intent = Intent(activity, SignInFragment::class.java)
-                intent.putExtra("email", account.email)
-                intent.putExtra("displayName", account.displayName)
-                startActivity(intent)
+            if (it.isSuccessful) {
+                Toast.makeText(activity, "Sign in successful.", Toast.LENGTH_SHORT).show()
                 view.findNavController().navigate(R.id.action_signInFragment_to_preferencesFragment)
-            }else{
+            }
+            else {
                 Toast.makeText(activity, it.exception.toString(), Toast.LENGTH_SHORT).show()
             }
         }
     }
-
-
 }
