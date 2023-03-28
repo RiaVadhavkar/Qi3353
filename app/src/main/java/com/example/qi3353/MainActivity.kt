95770c06ddbd9108701341eba621fbc3d3b3df9b
JennaKrebs <jennakrebs@vt.edu>
2023-03-28 16:55:02 -0400

    push notifications

--- app/src/main/java/com/example/qi3353/MainActivity.kt
+++ app/src/main/java/com/example/qi3353/MainActivity.kt
@@ -3,9 +3,15 @@ package com.example.qi3353
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+
 }
