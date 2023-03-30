59bfdb323f37699d1e37321357604ed0995fd665
Ria Vadhavkar <vadhavkar.ria@gmail.com>
2023-03-30 18:12:32 -0400

    fixed splash screen timing

--- app/src/main/java/com/example/qi3353/MainActivity.kt
+++ app/src/main/java/com/example/qi3353/MainActivity.kt
@@ -2,18 +2,23 @@ package com.example.qi3353
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.ViewTreeObserver
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.firebase.database.core.view.View
 
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
+
+        var keepSplashOnScreen = true
+        val delay = 2000L
+
+        installSplashScreen().setKeepOnScreenCondition { keepSplashOnScreen }
+        Handler(Looper.getMainLooper()).postDelayed({ keepSplashOnScreen = false }, delay)
         setContentView(R.layout.activity_main)
-
-
     }
-
-
 }
