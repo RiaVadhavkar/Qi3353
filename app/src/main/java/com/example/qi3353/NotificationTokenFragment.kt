a4331de60cac0f26bc84e5ad072d8067e3b95cae
JennaKrebs <jennakrebs@vt.edu>
2023-04-02 01:51:48 -0400

    sending notifications on database updates

--- app/src/main/java/com/example/qi3353/NotificationTokenFragment.kt
+++ app/src/main/java/com/example/qi3353/NotificationTokenFragment.kt
@@ -1,21 +1,19 @@
 package com.example.qi3353
 
+
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
-import com.example.qi3353.databinding.FragmentSettingsBinding
-
-import java.util.*
-
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.qi3353.databinding.NotificationTokenBinding
-import com.google.firebase.iid.FirebaseInstanceIdReceiver
-import com.google.firebase.iid.internal.FirebaseInstanceIdInternal
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.gson.Gson
 import kotlinx.coroutines.CoroutineScope
@@ -25,12 +23,15 @@ import kotlinx.coroutines.launch
 const val TOPIC = "/topics/myTopic"
 
 class NotificationTokenFragment : Fragment() {
+    private val model: EventViewModel by viewModels()
+
     val TAG = "NotificationTokenFragment"
     private lateinit var binding: NotificationTokenBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         FirebaseService.sharedPref = activity?.getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
         FirebaseMessaging.getInstance().token.addOnSuccessListener {
             FirebaseService.token = it
@@ -48,8 +49,39 @@ class NotificationTokenFragment : Fragment() {
     ): View? {
         binding = NotificationTokenBinding.inflate(inflater, container, false)
         var view = binding.root
-        binding.btnSend.setOnClickListener{
-            val title = binding.etTitle.text.toString()
+        binding.btnSend.setOnClickListener {
+            var title = "" //= "oh crap" //binding.etTitle.text.toString()
+            // Create the observer which updates the UI.
+            //val nameObserver = Observer<MutableList<Event>> {
+            //}
+
+            // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
+            //model.eventListLiveData.observe(this, nameObserver)
+
+            val firebaseDatabase = FirebaseDatabase.getInstance()
+            val reference = firebaseDatabase.getReference()
+            reference.child("myTopic/topic-name")
+                .addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        val children = dataSnapshot.children
+
+                        children.forEach {
+                            //Toast.makeText(activity, it.toString(), Toast.LENGTH_LONG).show()
+
+                            val body = dataSnapshot.child("body").value.toString()
+                            title = dataSnapshot.child("title").value.toString()
+
+                        }
+
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+
+                    }
+
+                }
+                )
+
             val message = binding.etMessage.text.toString()
             val recipientToken = binding.etToken.text.toString()
             if (title.isNotEmpty() && message.isNotEmpty() && recipientToken.isNotEmpty()) {
@@ -66,6 +98,7 @@ class NotificationTokenFragment : Fragment() {
     private fun sendNotification(notification: PushNotification) = CoroutineScope(Dispatchers.IO).launch{
         try {
             val response = RetrofitInstance.api.postNotification(notification)
+
             if(response.isSuccessful) {
                 Log.d(TAG, "Response: ${Gson().toJson(response)}")
 
